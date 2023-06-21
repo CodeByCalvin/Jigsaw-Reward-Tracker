a9cb5f6fc8890042cf356a9a618a7f4abdb82384
CodeByCalvin <calvin@calvinmerry.com>
2023-06-21 18:29:16 +0100

    Added customisation options for the user in the sidebar component
    Classname
    Start/end date
    Number of horizontal/vertical pieces
    Jigsaw piece size
    Upload image
    Print certificate
    
    (More to come)

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,8 +9,29 @@ function Sidebar(props) {
     props.onVerticalPiecesChange(parseInt(event.target.value));
   }
 
+  function handleClassNameChange(event) {
+    props.onClassNameChange(event.target.value);
+  }
+
   return (
     <div className="sidebar">
+      <label htmlFor="classname">Class name:</label>
+      <input
+        type="text"
+        id="classname"
+        className="classname"
+        value={props.className}
+        onChange={handleClassNameChange}
+      />
+      <br></br>
+      <label htmlFor="startdate">Start Date:</label>
+      <input type="date" id="startdate" className="startdate" />
+
+      <br></br>
+      <label htmlFor="startdate">End Date:</label>
+      <input type="date" id="startdate" className="startdate" />
+
+      <br></br>
       <label htmlFor="horizontalPiecesInput">
         Number of horizontal pieces:
       </label>
@@ -19,16 +40,37 @@ function Sidebar(props) {
         id="horizontalPiecesInput"
         className="horizontal-pieces-input"
         value={props.horizontalPieces}
+        defaultValue={3}
         onChange={handleHorizontalPiecesChange}
       />
+      <br></br>
       <label htmlFor="verticalPiecesInput">Number of vertical pieces:</label>
       <input
         type="number"
         id="verticalPiecesInput"
         className="vertical-pieces-input"
         value={props.verticalPieces}
+        defaultValue={3}
         onChange={handleVerticalPiecesChange}
       />
+      <br></br>
+      <label htmlFor="jigsawPieceSize">Jigsaw piece pieceSize:</label>
+      <input
+        type="number"
+        id="jigsawPieceSize"
+        className="jigsaw-piece-size"
+        defaultValue={150}
+      />
+      <br></br>
+      <label htmlFor="imageInput">Upload image:</label>
+      <input
+        type="file"
+        id="imageInput"
+        className="image-input"
+        onChange={props.onImageChange}
+      />
+      <br></br>
+      <button onClick={props.onPrint}>Print Certicate</button>
     </div>
   );
 }
