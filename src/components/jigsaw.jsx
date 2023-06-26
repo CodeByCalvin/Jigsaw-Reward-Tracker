bdafe0a530e245e367be676996d67fb677e95ff5
CodeByCalvin <calvin@calvinmerry.com>
2023-06-26 21:35:56 +0100

    By default, the jigsaw loads with a default image (unless the user uploads a custom image)

--- src/components/jigsaw.jsx
+++ src/components/jigsaw.jsx
@@ -1,44 +1,43 @@
-import React, { useState, useEffect, useRef } from "react";
-import Sidebar from "./sidebar";
+import React, { useEffect } from "react";
+import headbreaker from "headbreaker";
+import { useState } from "react";
 
 const Jigsaw = (props) => {
-  const puzzleRef = useRef();
-  const [pieceSize, setPieceSize] = useState(100);
-
+  // Jigsaw with image
   useEffect(() => {
-    const Canvas = window.headbreaker.Canvas;
+    const backgroundImg = new Image();
+    backgroundImg.src = props.image || "";
+    backgroundImg.onload = () => {
+      const autogen = new headbreaker.Canvas("autogen-canvas", {
+        width: 800,
+        height: 650,
+        pieceSize: props.pieceSize,
+        proximity: 20,
+        borderFill: 10,
+        strokeWidth: 1.5,
+        lineSoftness: 0.18,
+        image: backgroundImg,
+      });
 
-    const autogen = new Canvas(puzzleRef.current, {
-      width: 2000,
-      height: 650,
-      pieceSize: 150,
-      proximity: 5,
-      borderFill: 10,
-      strokeWidth: 2,
-      lineSoftness: 0.18,
-    });
+      autogen.adjustImagesToPuzzleHeight();
+      autogen.autogenerate({
+        horizontalPiecesCount: props.horizontalPieces,
+        verticalPiecesCount: props.verticalPieces,
+      });
+      autogen.draw();
+    };
+  }, [
+    props.horizontalPieces,
+    props.verticalPieces,
+    props.pieceSize,
+    props.image,
+  ]);
 
-    const metadata = [
-      { color: "#B83361" },
-      { color: "#B87D32" },
-      { color: "#A4C234" },
-      { color: "#37AB8C" },
-    ];
-
-    const metadataArray = Array.from(
-      { length: props.horizontalPieces * props.verticalPieces },
-      (_, i) => metadata[i % metadata.length]
-    );
-
-    autogen.autogenerate({
-      horizontalPiecesCount: props.horizontalPieces,
-      verticalPiecesCount: props.verticalPieces,
-      metadata: metadataArray,
-    });
-
-    autogen.draw();
-  }, [props.horizontalPieces, props.verticalPieces, pieceSize]);
-
-  return <div ref={puzzleRef}></div>;
+  return (
+    <div>
+      <div id="autogen-canvas"></div>
+    </div>
+  );
 };
+
 export default Jigsaw;
